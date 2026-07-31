9f6a2d4eba2da2a8e289ffc8f7913d2cd2f3d394
Mridul <mmridul@docsndocs.com>
2026-07-30 22:27:45 -0400

    Update multiple code fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   apiUrl: 'https://api.docsndocs.com/api',
   serverUrl: 'https://api.docsndocs.com',
   // Stripe publishable key (safe to expose in the browser). Use your LIVE key here.
-  stripePublishableKey: 'pk_live_51Tf2Bi9k8WLSuq9q1Ss8ttwqujucCENVolTTZibalO09fZJrlXP6AvZwD6G4rmxOpmFngEH66qulB1z1Q465Bgdx00CANXm5ne',
+  stripePublishableKey: 'pk_test_51Tf2DS5xCM5GUJuMwO8txE1v7VSOZDGPyjyymFYrhKfZyfkvtpw21BMDX3xy4iVkxZInNKMAXLe2Nao3aTtbZuTQ00RI29fShC',
+  //stripePublishableKey: 'pk_live_51Tf2Bi9k8WLSuq9q1Ss8ttwqujucCENVolTTZibalO09fZJrlXP6AvZwD6G4rmxOpmFngEH66qulB1z1Q465Bgdx00CANXm5ne',
 };
